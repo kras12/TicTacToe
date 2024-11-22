63813d37e2a8cd304283a4d379f74e8daef1377c
runehero <runehero@hotmail.com>
2024-11-23 00:50:12 +0100

    Added scaling to player text labels on the game board cells so that they scale with the cells as the main window size changes.

---
+++ TicTacToe/Converters/HeightToFontSizeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace TicTacToe.Converters;
+
+public class HeightToFontSizeConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is double actualHeight && parameter is string parameterString 
+            && double.TryParse(parameterString, NumberStyles.Any, CultureInfo.InvariantCulture, out double scale))
+        {
+            return actualHeight * scale;
+        }
+
+        throw new ArgumentException("The value and parameter must be of type double");
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
